620f7d3fde17d4d4bae2fe0cb99cea1eecc5126e
Reniel Mallari <renielmallari@Reniels-iMac.local>
2019-07-09 02:37:17 +0800

    testing

--- assets/scripts/qr.js
+++ assets/scripts/qr.js
@@ -14,12 +14,14 @@ function openQRCamera(node) {
   reader.readAsDataURL(node.files[0]);
   //Passing Data
   console.log(node.files[0].name);
-  var data1 = "testing"
+  var data1 = {
+	  'qr_url' : node.files[0].name
+  }
 	  $.ajax({
 		  url: "http://localhost:8080/qr_localhost/welcome/qr_add",
 		  type: "POST",
-		  ContentType: 'application/json',
-	  	data: {'qr_url': node.files[0].name},
+		  ContentType: 'application/x-www-form-urlencoded',
+	  	data:data1,
 			// application: "application/json",
 		  processData: false,
 		  success: function(data) {
